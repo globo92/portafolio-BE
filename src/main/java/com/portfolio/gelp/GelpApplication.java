6c48a6a846cbe1e275c2b28027ae6463ae0b5234
globo92 <gabrielelobop@gmail.com>
2022-12-03 20:34:18 -0300

    backend

---
+++ src/main/java/com/portfolio/gelp/GelpApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.gelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GelpApplication.class, args);
+	}
+
+}
